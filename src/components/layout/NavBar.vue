44e4ec18e61681d0272422ec4068cb999139ccb7
David Diomede <david@d2creativedesign.com>
2021-07-31 14:07:32 -0700

    Add nav item

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -3,7 +3,7 @@
         <v-list-item>
         <v-list-item-content>
             <v-list-item-title class="text-h6">
-            Outbreak
+            The Covid Online Board Game
             </v-list-item-title>
             <v-list-item-subtitle>
             subtext
@@ -13,24 +13,25 @@
 
         <v-divider></v-divider>
 
-        <v-list
-        dense
-        nav
-        >
+      <v-list
+          dense
+          nav
+      >
         <v-list-item
             v-for="item in items"
             :key="item.title"
             link
+            :to="item.to"
         >
-            <v-list-item-icon>
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
+          </v-list-item-icon>
 
-            <v-list-item-content>
+          <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
+          </v-list-item-content>
         </v-list-item>
-        </v-list>
+      </v-list>
     </v-navigation-drawer>
 </template>
 
@@ -39,6 +40,16 @@
     name: 'NavBar',
     props: {
         drawer: Boolean
-    }
+    },
+    data () {
+      return {
+        items: [
+          { title: 'Game', icon: 'mdi-virus', to: '/game' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
   }
 </script>
